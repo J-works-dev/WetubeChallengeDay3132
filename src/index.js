46495c4b5b25a6f9ad16e6e75a9d30b99b5d3147
SangJoon Lee(Jeremy) <sangjoon.jeremy.lee@gmail.com>
2020-12-17 22:30:20 +0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import "./styles.css";
+
+const recorderContainer = document.querySelector(".record-container");
+const recordBtn = document.querySelector(".recordBtn");
+const audio = document.querySelector("audio");
+
+const startRecording = () => {
+  navigator.mediaDevices
+    .getUserMedia({ video: false, audio: true })
+    .then(function (stream) {
+      audio.srcObject = MediaStream;
+    })
+    .catch(function (err) {
+      alert("Error: " + err);
+    });
+};
+
+function init() {
+  recordBtn.addEventListener("click", startRecording);
+}
+
+if (recorderContainer) {
+  init();
+}
